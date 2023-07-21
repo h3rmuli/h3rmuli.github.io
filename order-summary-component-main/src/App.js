ff88c300eb121ab9e0a57f8719e8ea530c3f2e0d
h3rmuli <niko.kangas@protonmail.com>
2023-07-21 19:21:29 +0300

    order summary UI fix

--- order-summary-component-main/src/App.js
+++ order-summary-component-main/src/App.js
@@ -3,7 +3,7 @@ import OrderSummary from './components/OrderSummary';
 
 const App = () => {
   return (
-    <div className="bg-pale-blue lg:bg-pattern-mobile lg:bg-no-repeat lg:min-h-screen">
+    <div className="bg-pale-blue lg:bg-pattern-desktop lg:bg-repeat-x lg:min-h-screen">
       <div className="p-4 lg:p-10 lg:max-w-md lg:mx-auto">
         <Hero />
         <OrderSummary />
