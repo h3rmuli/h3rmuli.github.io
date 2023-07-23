ce1ad0ba5bfc5f3976ab4af7f776ba4e7b5e04f5
h3rmuli <niko.kangas@protonmail.com>
2023-07-23 13:48:00 +0300

    nft preview card starter

---
+++ nft-preview-card-component/src/App.js
@@ -0,0 +1,16 @@
+const App = () => {
+  console.log('Hello from komponentti');
+  return (
+    <>
+      <div>
+        <p className="bg-slate-200 p-5">Hello world</p>
+        <p>
+          Equilibrium #3429 Our Equilibrium collection promotes balance and
+          calm. 0.041 ETH 3 days left Creation of Jules Wyvern
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default App;
