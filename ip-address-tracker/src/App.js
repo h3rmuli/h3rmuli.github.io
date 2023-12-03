7812bfc01b628aaa34fc592d3cf551b3d7e9fa0f
h3rmuli <niko.kangas@protonmail.com>
2023-12-03 15:03:32 +0100

    ip-tracker initial

---
+++ ip-address-tracker/src/App.js
@@ -0,0 +1,12 @@
+const App = () => {
+  console.log('Hello from komponentti');
+  return (
+    <>
+      <div>
+        <p className="bg-slate-200 p-5">Hello worldasd</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
